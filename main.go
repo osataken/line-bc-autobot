e2f2652924af31d2b0367cbdfee33934ba78577e
Kunaruk Osatpirat <osataken@gmail.com>
2016-05-13 20:42:26 +0700

    Add root url handler

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/", DefaultHandler)
 	r.HandleFunc("/message/relay", MessageRelayHandler)
 
 	// Bind to a port and pass our router in
 	http.ListenAndServe(":80", r)
+}
+
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!\n"))
 }
 
 func MessageRelayHandler(w http.ResponseWriter, r *http.Request) {
