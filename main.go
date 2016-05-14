7f01fa956552da72d4845d67adbfbd3ed34c0951
Kunaruk Osatpirat <osataken@gmail.com>
2016-05-14 13:48:12 +0700

    cast byte[] to string

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 func MessageRelayHandler(w http.ResponseWriter, r *http.Request) {
 	receivedMessage := message.Receive{}
 	if body, _ := ioutil.ReadAll(r.Body); len(body) > 0 {
-		fmt.Println(body)
+		fmt.Println(string(body))
 		err := json.Unmarshal(body, &receivedMessage)
 		if err != nil {
 			w.WriteHeader(400)
