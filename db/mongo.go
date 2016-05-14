f4f61ba394ba520c3e9ddba14b9e6f2bc16bfaa9
Nguan <nguansak@gmail.com>
2016-05-14 15:35:55 +0700

    Add monogodb_uri as env variables

--- db/mongo.go
+++ db/mongo.go
@@ -3,13 +3,16 @@ package db
 import (
 	"gopkg.in/mgo.v2"
 	"fmt"
+	"os"
 )
 
 var mongoSession *mgo.Session
 
 func InitDB() {
 	var err error
-	session, err := mgo.Dial("mongodb://line-bc-autobot:ilovedata@ds023042.mlab.com:23042/heroku_7x3w65x3")
+
+	url := os.Getenv("MONGODB_URI")
+	session, err := mgo.Dial(url)
 	//session, err := mgo.Dial("mongodb://192.168.99.100:27017/heroku_7x3w65x3")
 	if err != nil {
 		panic(err)
